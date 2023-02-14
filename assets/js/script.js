72e04c870c24b2c1f651637afac9d8f742ce21c7
AytajVV <aytajvv@code.edu.az>
2023-02-14 23:49:17 +0400

    added some lines

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,10 @@ function Check(num){
 let number=parseInt(prompt());
 console.log(Check(33));
 
+//Task1 arrow function
+let check=(num)=>{
+    let result=num%21==0?num/21 : `bolunmur`
+    console.log(result);
+}
+let num=parseInt(prompt());
+check(num);
